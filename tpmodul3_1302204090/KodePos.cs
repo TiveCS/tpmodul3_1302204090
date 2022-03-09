47487e0302ea07ef262e10d4991ea1f5d79cfd27
TiveCS <creativecode.id@gmail.com>
2022-03-09 13:09:21 +0700

    implementasi table driven

--- tpmodul3_1302204090/KodePos.cs
+++ tpmodul3_1302204090/KodePos.cs
@@ -10,21 +10,6 @@ namespace tpmodul3_1302204090
 
 		public KodePos()
 		{
-			/*
-		
-			Batununggal 40266
-			Kujangsari 40287
-			Mengger 40267
-			Wates 40256
-			Cijaura 40287
-			Jatisari 40286
-			Margasari 40286
-			Sekejati 40286
-			Kebonwaru 40272
-			Maleer 40274
-			Samoja 40273
-
-			 */
 			
 			addKodePos(40266, new string[] { "Batununggal" });
 			addKodePos(40287, new string[] { "Kujangsari", "Cijaura" });
